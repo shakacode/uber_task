94274892bcfc2f808c49af149f2ed7c0c347f426
Justin Gordon <justin@shakacode.com>
2022-06-16 15:28:17 -1000

    Initial commit

---
+++ spec/uber_task_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe UberTask do
+  it "has a version number" do
+    expect(UberTask::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
