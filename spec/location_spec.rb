8805bb0418632951d4380aa657fc73dbe0e118d8
Alexandre Borela <alexandre@borela.tech>
2022-06-21 10:35:39 -0300

    Update location tests

--- spec/location_spec.rb
+++ spec/location_spec.rb
@@ -4,28 +4,65 @@ require 'pathname'
 
 module UberTask
   describe Location do
-    it 'shortens paths' do
-      project_root = Pathname.new('/some-project')
-      stub_const('Rails', double(root: project_root))
+    describe '#path' do
+      context 'normal' do
+        let(:paths) do
+          [
+            '/foo/bar/baz/gems/bar',
+            '/foo/bar/rubygems/baz',
+          ]
+        end
 
-      shortened = [
-        project_root.join('foo').to_s,
-        '/something/gems/bar',
-        '/something/rubygems/baz',
-      ].map do |path|
-        Location.new(
-          path: path,
-          line: 42,
-        ).path
+        before do
+          stub_const('Rails', nil)
+        end
+
+        it 'shortens paths' do
+          shortened = paths.map do |path|
+            Location.new(
+              path: path,
+              line: 42,
+            ).path
+          end
+          expect(shortened).to eq(
+            %w[
+              [GEM]/bar
+              [GEM]/baz
+            ],
+          )
+        end
       end
 
-      expect(shortened).to eq(
-        %w[
-          [PROJECT]/foo
-          [GEM]/bar
-          [GEM]/baz
-        ],
-      )
+      context 'in rails' do
+        let(:rails_root) { Pathname.new(Dir.pwd) }
+        let(:paths) do
+          [
+            rails_root.join('abc').to_s,
+            '/foo/bar/baz/gems/bar',
+            '/foo/bar/rubygems/baz',
+          ]
+        end
+
+        before do
+          stub_const('Rails', double(root: rails_root))
+        end
+
+        it 'shortens paths' do
+          shortened = paths.map do |path|
+            Location.new(
+              path: path,
+              line: 42,
+            ).path
+          end
+          expect(shortened).to eq(
+            %w[
+              [PROJECT]/abc
+              [GEM]/bar
+              [GEM]/baz
+            ],
+          )
+        end
+      end
     end
   end
 end
