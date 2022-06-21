e3e60729476e0358eedaf416254c775bb465db7f
Alexandre Borela <alexandre@borela.tech>
2022-06-21 12:18:17 -0300

    Remove unneeded comment

--- lib/uber_task/event.rb
+++ lib/uber_task/event.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 module UberTask
-  # We are inheriting from Exception to make sure it won't be caught by accident
-  # which would affect the flow the tasks.
   class Event
     attr_reader :name
 
