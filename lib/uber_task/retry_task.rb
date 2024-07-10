15e613f11496c676836b9cd51a0c3ad072415239
Zakir Dzhamaliddinov <zakir.dzhamaliddinov@gmail.com>
2024-07-10 16:21:25 +0300

    Validate UberTask::RetryTask's wait value

--- lib/uber_task/retry_task.rb
+++ lib/uber_task/retry_task.rb
@@ -7,10 +7,19 @@ module UberTask
     attr_accessor :reason,
                   :wait
 
-    def initialize(reason: nil, wait: nil)
+    def initialize(reason: nil, wait: 0)
+      validate_wait_value!(wait)
+
       super('Requested to retry the task.')
       @reason = reason
       @wait = wait
+    end
+
+    private
+
+    def validate_wait_value!(value)
+      raise ArgumentError, '`wait` is not numberic' unless value.is_a?(Numeric)
+      raise ArgumentError, '`wait` cannot be negative' if value.negative?
     end
   end
 end
