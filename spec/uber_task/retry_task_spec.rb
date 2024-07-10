1c3efc422d436078a0361a3e4b0352e4f8a9b960
Zakir <zakir.dzhamaliddinov@gmail.com>
2024-07-10 17:11:55 +0300

    Validate UberTask::RetryTask's wait value (#17)

--- spec/uber_task/retry_task_spec.rb
+++ spec/uber_task/retry_task_spec.rb
@@ -20,7 +20,25 @@ describe UberTask::RetryTask do
     expect(exception.wait).to eq(42)
   end
 
+  it 'has 0 as a default wait attribute value' do
+    expect(described_class.new.wait).to eq(0)
+  end
+
   it 'has a message' do
     expect(exception.message).to eq('Requested to retry the task.')
+  end
+
+  describe 'wait attribute validations' do
+    it 'does not allow non-numeric value' do
+      expect do
+        described_class.new(wait: 'string')
+      end.to raise_error(ArgumentError, '`wait` is not numberic')
+    end
+
+    it 'does not allow negative value' do
+      expect do
+        described_class.new(wait: -1)
+      end.to raise_error(ArgumentError, '`wait` cannot be negative')
+    end
   end
 end
