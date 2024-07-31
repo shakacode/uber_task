b2b38914e72ee60915afbeecb92239898c065d3a
pedrinlopes <pedrinlopes@hotmail.com>
2024-07-30 23:04:56 -0300

    Fix cop rules

--- spec/non_rails/uber_task/task_skipped_on_error_spec.rb
+++ spec/non_rails/uber_task/task_skipped_on_error_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 describe UberTask::SkipTask do
   it 'Skips the task on a error' do
     task_skipped_after_exception = false
-  
+
     UberTask.run do
       UberTask.on_subtask_error do |_task, _event, err|
         if err.message == 'Some error'
@@ -10,11 +12,11 @@ describe UberTask::SkipTask do
         end
       end
 
-      UberTask.run do 
+      UberTask.run do
         raise 'Some error' if task_skipped_after_exception == false
       end
     end
-  
+
     expect(task_skipped_after_exception).to be(true)
   end
 end
